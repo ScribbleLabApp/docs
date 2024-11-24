f92662469ebc367636a84cc70ee20fde597118bc
N3v1 <n3v1010@gmail.com>
2024-11-24 21:35:51 +0100

    (chore): Add contribution guide and page placeholder

--- src/pages/_meta.ts
+++ src/pages/_meta.ts
@@ -6,6 +6,7 @@ export default {
   community: 'Community & Support',
   news: 'Changelog & Release Notes',
   troubleshooting: 'Troubleshooting & FAQ',
+  contributing: "Contributing",
   security: 'Security',
   legal: 'Legal & License',
   404: {
---
+++ src/pages/contributing/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+    index: "Contribution Guidelines",
+    documentation: "Adding new documentation",
+    codingstyle: "Coding Style Guidelines"
+}
